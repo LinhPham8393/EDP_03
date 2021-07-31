0d69276efedaf7ef76d64245f5c533d2cd3d394e
linhpham8393 <phamlinh8393@gmail.com>
2021-07-31 16:39:02 +0700

    Pham Manh Linh nop bai assignment 1(ban update)

--- SQL/Assignment1/Testing_System_Assignment_1.sql
+++ SQL/Assignment1/Testing_System_Assignment_1.sql
@@ -1,56 +1,86 @@
-
-
-create database linh;
-use linh;
+create database LinhPham;
+use LinhPham;
 
 create table Department(
-DepartmentID int AUTO_INCREMENT,
-DepartmentName varchar(50),
-PRIMARY KEY(DepartmentID)
-)
+	DepartmentID int AUTO_INCREMENT,
+	DepartmentName varchar(50),
+	PRIMARY KEY(DepartmentID)
+);
 
 create table Position1(
-PositionID int AUTO_INCREMENT,
-PositionName varchar(50),
-PRIMARY KEY(PositionID)
-)
+	PositionID int AUTO_INCREMENT,
+	PositionName varchar(50),
+	PRIMARY KEY(PositionID)
+);
 
 create table Account(
-AccountID int AUTO_INCREMENT,
-Email varchar(50),
-Username varchar(50),
-FullName varchar(50),
-DepartmentID int,
-PositionID int,
-CreateDate datetime,
-PRIMARY KEY(AccountID)
-)
-
-Table 4: Group
- GroupID: định danh của nhóm (auto increment)
- GroupName: tên nhóm
- CreatorID: id của người tạo group
- CreateDate: ngày tạo group
-
+	AccountID int AUTO_INCREMENT,
+	Email varchar(50),
+	Username varchar(50),
+	FullName varchar(50),
+	DepartmentID int,
+	PositionID int,
+	CreateDate datetime,
+	PRIMARY KEY(AccountID)
+);
 
 create table Group1(
-GroupID int AUTO_INCREMENT,
-GroupName varchar(50),
-CreatorID varchar(50),
-CreateDate datetime,
-PRIMARY KEY(GroupID)
-)
-
-Table 5: GroupAccount
- GroupID: định danh của nhóm
- AccountID: định danh của User
- JoinDate: Ngày user tham gia vào nhóm
+	GroupID int AUTO_INCREMENT,
+	GroupName varchar(50),
+	CreatorID varchar(50),
+	CreateDate datetime,
+	PRIMARY KEY(GroupID)
+);
 
 create table GroupAccount(
-GroupID int ,
-AccountID varchar(50),
-JoinDate datetime
-)
+	GroupID int ,
+	AccountID varchar(50),
+	JoinDate datetime
+);
 
+create table TypeQuestion(
+	TypeID int AUTO_INCREMENT,
+	TypeName varchar(50),
+	PRIMARY KEY(TypeID)
+);
 
+create table CategoryQuestion(
+	CategoryID int AUTO_INCREMENT,
+	CategoryName varchar(50),
+	PRIMARY KEY(CategoryID)
+);
+
+create table Question(
+	QuestionID int AUTO_INCREMENT,
+	Content varchar(1000),
+    CategoryID int,
+    TypeID int, 
+    CreatorID int,
+    CreateDate datetime,
+	PRIMARY KEY(QuestionID)
+);
+
+create table Answer(
+	AnswerID int AUTO_INCREMENT,
+	Content varchar(1000),
+    QuestionID int,
+    isCorrect varchar(50),
+	PRIMARY KEY(AnswerID)
+);
+
+create table Exam(
+	ExamID int AUTO_INCREMENT,
+	Code1 varchar(1000),
+    Title varchar(1000),
+    CategoryID int,
+    Duration varchar(50),
+    CreatorID int,
+    CreateDate datetime,
+	PRIMARY KEY(ExamID)
+);
+
+create table ExamQuestion(
+	ExamID int,
+	QuestionID int
+);
 
